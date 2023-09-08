07d980ed81184b356c04c7b30d31d4651634d91c
Gleb Milovanov <milgleb@gmail.com>
2023-09-08 16:39:36 +0300

    fix: logout

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -32,7 +32,7 @@ export const login = async (email, password) => {
   return _getResponseData(response);
 };
 
-export const logout = async () => {
+/* export const logout = async () => {
   const response = await fetch(`${BASE_URL}/logout`, {
     method: 'POST',
     headers: {
@@ -41,7 +41,7 @@ export const logout = async () => {
     credentials: 'include',
   });
   return _getResponseData(response);
-};
+}; */
 
 export const checkToken = async () => {
   const response = await fetch(`${BASE_URL}/users/me`, {
